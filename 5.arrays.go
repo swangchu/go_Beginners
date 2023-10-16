d9faa39780c8c00212b17571a0fd3ae9634735a9
Sonam Wangchu <swangchu79@gmail.com>
2023-10-16 10:09:46 +0600

    Create 5.arrays.go

---
+++ 5.arrays.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fruits [2]string = [2]string{"apples", "oranges"}
+	fmt.Println(fruits)
+
+	marks := [3]int{10, 20, 30}
+	fmt.Println(marks)
+
+	names := [...]string{"sonam", "Cheten", "Geduen"}
+	fmt.Println(names)
+}
